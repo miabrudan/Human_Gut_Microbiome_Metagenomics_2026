295874e71a61bf48d71f51329797e3532233b62e
monicaiabrudan <161705093+monicaiabrudan@users.noreply.github.com>
2026-06-23 16:36:40 +0100

    removed the data

--- course_data_2026/Module5a/scripts/merge_pathabundance.sh
+++
@@ -1,6 +0,0 @@
-module load humann3/3.8
-
-humann_join_tables \
-  --input ./ \
-  --output humann3_pathabundance_merged_normalised_UG.tsv \
-  --file_name pathabundance_relab.tsv
--- course_data_2026/Module5a/scripts/merge_pathcoverage.sh
+++
@@ -1,6 +0,0 @@
-module load humann3/3.8
-
-humann_join_tables \
-  --input . \
-  --file_name pathcoverage \
-  --output humann3_pathcoverage_merged_UG.tsv
